4a853386489a3c2e4b921fd36c69cc13a9d186c7
Sondre Sjølyst <18713420+sondresjolyst@users.noreply.github.com>
2026-04-18 22:35:48 +0200

    fix: add missing error message on registrer (#175) (#176)

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -35,6 +35,7 @@ const registerSchema = z.object({
         .min(8, { message: 'Be at least 8 characters long' })
         .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
         .regex(/[0-9]/, { message: 'Contain at least one number.' })
+        .regex(/[A-Z]/, { message: 'Contain at least one uppercase letter.' })
         .regex(/[^a-zA-Z0-9]/, {
             message: 'Contain at least one special character.',
         })
